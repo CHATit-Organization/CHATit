36b86b1c8c690fb494aedd872830e89978b0f544
Mohzcrea8me <mohzcrea8me@gmail.com>
2023-07-03 13:55:48 +0800

    feat: bug fix

--- src/components/TransactionFailed.jsx
+++ src/components/TransactionFailed.jsx
@@ -6,7 +6,7 @@ import { store } from "../store/zustand";
 function TransactionFailed() {
   const storeState = store((state) => state);
   return (
-    <div className="transactionFailed-container w-[300px] flex flex-col items-center justify-between gap-[30px]">
+    <div className="transactionFailed-container flex flex-col items-center justify-between gap-[30px]">
       <img src={failed} alt="" />
       <div className="transactionFailed-header font-bold text-xl">
         <h2>Transaction Failed</h2>
@@ -16,7 +16,9 @@ function TransactionFailed() {
       </p>
       <button
         className={`flex w-[100px] h-[30px] bg-[#7758d1] rounded-lg items-center justify-center hover:bg-opacity-75 active:bg-opacity-60 shadow-2xl`}
-        onClick={() => {storeState.setTransferStatus(null)}}
+        onClick={() => {
+          storeState.setTransferStatus(null);
+        }}
       >
         Done
       </button>
